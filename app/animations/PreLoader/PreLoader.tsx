fd0e4af9b4dd54f40dc880eb1534f8087fe9fb1a
Anas Farooq <anasfarooq2006@gmail.com>
2024-11-08 04:09:29 +0500

    Update PreLoader.tsx

--- app/animations/PreLoader/PreLoader.tsx
+++ app/animations/PreLoader/PreLoader.tsx
@@ -11,8 +11,8 @@ const PreLoader = () => {
     <div className='preloader gap-[5px] overflow-hidden text-[14px] sm:gap-[10px] sm:text-[18px] md:text-[18px] lg:text-[20px]'>
         <div className='text-container w-500 flex h-60 items-center opacity-0 justify-center gap-[5px] overflow-hidden text-[14px] font-extrabold sm:gap-[10px] sm:text-[16px] md:text-[18px] lg:text-[20px]'>
             <span>Developer.</span>
-            <span>Programmer.</span>
-            <span>Coder.</span>
+            <span>Designer.</span>
+            <span>Freelancer.</span>
             <div className="sub hidden"></div>
         </div>
     </div>
